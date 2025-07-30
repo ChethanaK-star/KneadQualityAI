89a5318f01d0ed84091fdfa6fcc0e460dbea23a4
ChethuK-star <chethanakanakala54@gmail.com>
2025-07-30 13:56:59 +0000

    Master fkery Ultra

--- frontend/src/utils.js
+++
@@ -1,9 +0,0 @@
-// utils.js
-export function getUserId() {
-  let uid = localStorage.getItem("user_id");
-  if (!uid) {
-    uid = crypto.randomUUID();
-    localStorage.setItem("user_id", uid);
-  }
-  return uid;
-}
